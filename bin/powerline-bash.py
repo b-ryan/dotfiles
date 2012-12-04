3a82050dcc1b4bfa3e2dfb214d5eb8c85076e19b
Buck Ryan <buck.ryan@gmail.com>
2012-12-04 17:34:25 -0500

    updated powerline

--- bin/powerline-bash.py
+++ bin/powerline-bash.py
@@ -108,7 +108,7 @@ def add_cwd_segment(powerline, cwd, maxdepth, cwd_only = False):
     if not cwd_only:
         for n in names[:-1]:
             powerline.append(Segment(powerline, ' %s ' % n, Color.PATH_FG, Color.PATH_BG, powerline.separator_thin, Color.SEPARATOR_FG))
-        powerline.append(Segment(powerline, ' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG))
+    powerline.append(Segment(powerline, ' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG))
 
 def get_hg_status():
     has_modified_files = False
