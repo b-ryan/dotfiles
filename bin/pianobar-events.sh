8a5a7f39b7f33d76c76222fce5533b003e4ef9fc
Buck Ryan <buck.ryan@gmail.com>
2012-12-04 16:31:55 -0500

    cleaned up the pianobar events file

--- bin/pianobar-events.sh
+++ bin/pianobar-events.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
 
 OUTFILE=~/.config/pianobar/output
+PAUSED="(Paused)"
+THUMBS_UP="<3"
 
 # create variables
 while read L; do
     k="`echo "$L" | cut -d '=' -f 1`"
     v="`echo "$L" | cut -d '=' -f 2`"
     export "$k=$v"
-done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
+done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin)
 
-xmobar () {
-    echo -n $@ > $OUTFILE
-}
+xmobar () { echo -n $@ > $OUTFILE; }
 
 case "$1" in
     songstart | songlove | playpause | playresume)
-        [ "$1" = "playpause" ] && pre="(Paused) "
-        [ "$rating" -eq 1 ] && post=" <3"
+        [ "$1" = "playpause" ] && pre="$PAUSED "
+        [ "$rating" -eq 1 ] && post=" $THUMBS_UP"
         xmobar $pre\"$title\" by \"$artist\"$post
         ;;
 
     songfinish)
         > $OUTFILE
         ;;
-
-#    songshelf)
-#        kdialog --title pianobar --passivepopup "SHELVING '$title' by '$artist' on '$album' on station '$stationName'" 10
-#        ;;
-
-#    songban)
-#        kdialog --title pianobar --passivepopup "BANNING '$title' by '$artist' on '$album' on station '$stationName'" 10
-#        ;;
-
-#    songbookmark)
-#        kdialog --title pianobar --passivepopup "BOOKMARKING '$title' by '$artist' on '$album'" 10
-#        ;;
-
-#    artistbookmark)
-#        kdialog --title pianobar --passivepopup "BOOKMARKING '$artist'" 10
-#        ;;
-
 esac
-
