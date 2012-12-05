5b6b24fe5385e53b51fb29186a604f0ee437c2b3
Buck Ryan <buck.ryan@gmail.com>
2012-12-05 12:46:06 -0500

    Added notify-send with album art for pianobar

--- bin/pianobar-events.sh
+++ bin/pianobar-events.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OUTFILE=~/.config/pianobar/output
+ARTFILE=~/.config/pianobar/albumart
 PAUSED="(Paused)"
 THUMBS_UP="<3"
 
@@ -12,12 +13,14 @@ while read L; do
 done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin)
 
 xmobar () { echo -n $@ > $OUTFILE; }
+fetchCoverArt() { wget -O $ARTFILE $coverArt; echo $ARTFILE; }
 
 case "$1" in
     songstart | songlove | playpause | playresume)
         [ "$1" = "playpause" ] && pre="$PAUSED "
         [ "$rating" -eq 1 ] && post=" $THUMBS_UP"
         xmobar $pre\"$title\" by \"$artist\"$post
+        notify-send --icon=$(fetchCoverArt) "$title" "by $artist"
         ;;
 
     songfinish)
