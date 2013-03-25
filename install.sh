ca796b0fdf22f19795b03305df20dae23f1178d7
Buck Ryan <buck.ryan@gmail.com>
2013-03-25 14:28:54 -0400

    Improved install script to make backups

--- install.sh
+++ install.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-LINK_FLAGS="-s"
-if [ "$1" = "--force" ]
-then
-    LINK_FLAGS="-sf"
-fi
-DIR_LINK_FLAGS="${LINK_FLAGS}n"
+LINK_FLAGS="--symbolic"
+DIR_LINK_FLAGS="${LINK_FLAGS}"
 
-mkhomedir() {
-    mkdir -p ~/$1 2> /dev/null || true
+mymkdir() {
+    mkdir -p $1 2> /dev/null || true
+}
+
+BK_DIR=$(pwd)/.bk/$(date '+%FT%T')
+mymkdir $BK_DIR
+backup() {
+    mv $1 $BK_DIR/
 }
 
 # arguments:
 # 1 flags to the ln command
 # 2 the local file in this directory that will be linked
-# 3 (optional) the destination relative to the home directory
-#   for example, instead of /home/user/.vim, this arguments would
-#   just be .vim. If no argument given, the destination will be
-#   the same as the source, but prepended with a dot.
+# 3 (optional) the destination. If no argument given, the destination will
+#   be the same as the source, but prepended with a dot and put in ~
 create_link() {
     FLAGS=$1
     SOURCE_REL=$2
     SOURCE_ABS=`pwd`/$2
     LINK=$3
-    [ $LINK ] || LINK=.$SOURCE_REL
-    ln $FLAGS $SOURCE_ABS ~/$LINK && echo "created link to '$SOURCE_ABS'"
+    [ $LINK ] || LINK=~/.$SOURCE_REL
+    backup $LINK
+    ln $FLAGS $SOURCE_ABS $LINK && echo "created link to '$SOURCE_ABS'"
 }
 symlink() {
     create_link $LINK_FLAGS $1 $2
@@ -32,10 +33,10 @@ dirlink() {
     create_link $DIR_LINK_FLAGS $1 $2
 }
 
-mkhomedir bin
-mkhomedir .xmonad
-mkhomedir .config/pianobar
-mkfifo ~/.config/pianobar/ctl
+mymkdir ~/bin
+mymkdir ~/.xmonad
+mymkdir ~/.config/pianobar
+mkfifo ~/.config/pianobar/ctl 2> /dev/null
 
 dirlink vim
 symlink vimrc
@@ -47,12 +48,12 @@ symlink gitignore
 symlink hgrc
 
 for file in $(ls bin); do
-    symlink bin/$file bin/$file
+    symlink bin/$file ~/bin/$file
 done
 
 symlink xmobarrc
-symlink xmonad/xmonad.hs .xmonad/xmonad.hs
+symlink xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 
-symlink pianobar/config .config/pianobar/config
+symlink pianobar/config ~/.config/pianobar/config
 
-true
+echo Backed up files to $BK_DIR
