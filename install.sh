7cd334cf0b87d14efeb2b65ea26175a7be6ae9bb
Buck Ryan <buck.ryan@gmail.com>
2013-06-10 09:24:45 -0400

    updated install script for fish

--- install.sh
+++ install.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 LINK_FLAGS="--symbolic"
-DIR_LINK_FLAGS="${LINK_FLAGS}"
+DIR_LINK_FLAGS="${LINK_FLAGS} --no-dereference"
 
 mymkdir() {
     mkdir -p $1 2> /dev/null || true
 }
 
-BK_DIR=$(pwd)/.bk/$(date '+%FT%T')
-mymkdir $BK_DIR
 backup() {
     mv $1 $BK_DIR/
 }
+
+BK_DIR=$(pwd)/.bk/$(date '+%FT%T')
+mymkdir $BK_DIR
 
 # arguments:
 # 1 flags to the ln command
 # 2 the local file in this directory that will be linked
 # 3 (optional) the destination. If no argument given, the destination will
 #   be the same as the source, but prepended with a dot and put in ~
-create_link() {
-    FLAGS=$1
-    SOURCE_REL=$2
-    SOURCE_ABS=`pwd`/$2
-    LINK=$3
-    [ $LINK ] || LINK=~/.$SOURCE_REL
-    backup $LINK
-    ln $FLAGS $SOURCE_ABS $LINK && echo "created link to '$SOURCE_ABS'"
+_create_link() {
+    local flags=$1
+    local source_rel=$2
+    local source_abs=$(pwd)/$2
+    local link=$3
+
+    [ $link ] || link=~/.$source_rel
+    backup $link
+    ln $flags $source_abs $link && echo "created link to '$source_abs'"
 }
 symlink() {
-    create_link $LINK_FLAGS $1 $2
+    _create_link "$LINK_FLAGS" $1 $2
 }
 dirlink() {
-    create_link $DIR_LINK_FLAGS $1 $2
+    _create_link "$DIR_LINK_FLAGS" $1 $2
 }
 
 mymkdir ~/bin
 mymkdir ~/.xmonad
 mymkdir ~/.config/pianobar
+mymkdir ~/.config/fish
 mkfifo ~/.config/pianobar/ctl 2> /dev/null
 
 dirlink vim
@@ -54,6 +57,7 @@ done
 symlink xmobarrc
 symlink xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 
-symlink pianobar/config ~/.config/pianobar/config
+symlink config/pianobar/config ~/.config/pianobar/config
+symlink config/fish/config.fish ~/.config/fish/config.fish
 
 echo Backed up files to $BK_DIR
