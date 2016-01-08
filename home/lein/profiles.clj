862f08840e8c648f6b0d53db91dd760470305287
Buck Ryan <fryan@rjmetrics.com>
2016-01-08 13:38:16 -0500

    add clojure trace to profile'

--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]]
-        :dependencies [[slamhound "1.5.5"]]}}
+        :dependencies [[slamhound "1.5.5"]
+                       [org.clojure/tools.trace "0.7.9"]]}}
