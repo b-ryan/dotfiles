b7dee9f54d965f3567aded6f919b9c727673d09a
Buck Ryan <fryan@rjmetrics.com>
2016-03-22 12:52:44 -0400

    lein cljfmt, undo comment char, readability in alias

--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]
-                  [lein-ancient "0.6.8"]]
+                  [lein-ancient "0.6.8"]
+                  [lein-cljfmt  "0.4.1"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]
                        [slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :cljfmt  {:indents  {facts  [[:inner 0]]
+                             fact  [[:inner 0]]
+                             fact-group [[:inner 0]]}}}}
