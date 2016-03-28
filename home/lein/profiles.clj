6d88c17367fb4552a55bf892cc5e29103f5b354b
Buck Ryan <fryan@rjmetrics.com>
2016-03-28 11:50:13 -0400

    blah

--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]
                   [lein-ancient "0.6.8"]
-                  [lein-cljfmt  "0.4.1"]]
+                  [lein-cljfmt  "0.4.1"]
+                  [lein-try "0.4.3"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]
                        [slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
