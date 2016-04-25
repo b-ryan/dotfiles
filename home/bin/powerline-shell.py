e5d4d8ab129b2d745bb9fb61a67334adecf25e58
Buck Ryan <buck.ryan@gmail.com>
2016-04-25 10:38:49 -0400

    remove some segments from shell'

--- home/bin/powerline-shell.py
+++ home/bin/powerline-shell.py
@@ -308,39 +308,6 @@ def add_username_segment(powerline):
 
 
 add_username_segment(powerline)
-def add_hostname_segment(powerline):
-    if powerline.args.colorize_hostname:
-        from lib.color_compliment import stringToHashToColorAndOpposite
-        from lib.colortrans import rgb2short
-        from socket import gethostname
-        hostname = gethostname()
-        FG, BG = stringToHashToColorAndOpposite(hostname)
-        FG, BG = (rgb2short(*color) for color in [FG, BG])
-        host_prompt = ' %s ' % hostname.split('.')[0]
-
-        powerline.append(host_prompt, FG, BG)
-    else:
-        if powerline.args.shell == 'bash':
-            host_prompt = ' \\h '
-        elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m '
-        else:
-            import socket
-            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
-
-        powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
-
-
-add_hostname_segment(powerline)
-import os
-
-def add_ssh_segment(powerline):
-
-    if os.getenv('SSH_CLIENT'):
-        powerline.append(' %s ' % powerline.network, Color.SSH_FG, Color.SSH_BG)
-
-
-add_ssh_segment(powerline)
 import os
 
 ELLIPSIS = u'\u2026'
@@ -434,16 +401,6 @@ def add_cwd_segment(powerline):
 
 
 add_cwd_segment(powerline)
-import os
-
-def add_read_only_segment(powerline):
-    cwd = powerline.cwd or os.getenv('PWD')
-
-    if not os.access(cwd, os.W_OK):
-        powerline.append(' %s ' % powerline.lock, Color.READONLY_FG, Color.READONLY_BG)
-
-
-add_read_only_segment(powerline)
 import re
 import subprocess
 import os
