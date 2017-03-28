dbf2b7945aeca4b34e22d27a558620151ba4e884
Buck Ryan <buck.ryan@gmail.com>
2017-03-28 09:03:05 -0400

    codox lein profile

--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,9 +1,11 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]
                   [lein-ancient "0.6.8"]
                   [lein-cljfmt  "0.5.1"]
+                  [lein-codox "0.10.3"]
                   [lein-try "0.4.3"]
                   [lein-virgil "0.1.0"]
                   [jonase/eastwood "0.2.3"]]
+        :codox {:output-path "codox"}
         :dependencies [[org.clojure/tools.trace "0.7.9"]
                        [slamhound "1.5.5"]
                        [cljfmt "0.5.1"]]
